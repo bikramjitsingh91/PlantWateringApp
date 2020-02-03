0fe1c898776c9a1e721982b09c2803758ccaf650
bikramjitsingh91 <bikram.jit.singh@outlook.com>
2020-02-02 23:10:59 -0500

    add-6-hrs-event

--- plant-maintenance-app/src/app/app.component.ts
+++ plant-maintenance-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef, HostListener, Injectable } from '@angular/core';
 import { PlantInfoServices } from './plant-info.services';
-import { Observable, of } from 'rxjs';
+import { Observable, of, interval } from 'rxjs';
 import { PlantInfoModel, PlantInfoModelList } from 'src/model/plant-info-model';
 import { element } from '@angular/core/src/render3';
 
@@ -15,12 +15,19 @@ export class AppComponent {
   title = 'plant-maintenance-app';
   plants = [1,2,3,4,5];
   wateringNow: boolean[];
+  plantWatered: boolean[];
   timer = [];
   data: any;// = new Array<PlantInfoModel>();
   timeInterval = new Array();
   buttonDisableTimer = new Array();
+  timeToWaterPlant = new Array();
+  timeIntervelWaterPlantReset = new Array();
   startWatering = "Start watering";
   stopWatering = "Stop watering";
+  plantWateringStatusTracker: any;
+  remindToWater: number = 21600000 //6 hrs
+  timeTodisableWateringAfterWatering = 30000 //30 sec
+  timeToWater = 10000 //10 sec
   @ViewChild('wateringButton') wateringButton: ElementRef;
 
  
@@ -29,45 +36,49 @@ export class AppComponent {
   }
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     this.plantInfoServices.getPlantInfo<PlantInfoModelList>().subscribe(d => {
       this.data = d;
       this.wateringNow = new Array<boolean>(this.data.length).fill(false);
+      this.plantWatered = new Array<boolean>(this.data.length).fill(false);
       this.buttonDisableTimer = new Array<boolean>(this.data.length).fill(false);
       this.timer = new Array(this.data.length).fill(10);
+      this.timeIntervelWaterPlantReset = new Array(this.data.length).fill(this.remindToWater);
+      this.plantWateringStatusTracker = new Array(this.data.length);
       this.timeInterval = new Array(this.data.length);
-      console.log(this.wateringNow);
+      this.timeToWaterPlant = new Array(this.data.length);
+
+
+      setInterval (() => {
+        for(let i = 0; i < this.timeToWaterPlant.length; i++)
+        {
+          if(this.plantWatered[i]){
+            this.plantWatered[i] = false;
+            this.plantWateringStatusTracker[i] = false;
+          }
+          else
+            this.plantWateringStatusTracker[i] = true;
+        }
+      }, this.remindToWater);
     })
 
     
   }
 
-  ngAfterViewInit() {
-    console.log(this.wateringButton.nativeElement.innerHTML);
-  }
-
   onWateringStatusChanged(plantId){
     this.wateringNow[plantId] = !this.wateringNow[plantId];
+    this.plantWatered[plantId] = true;
+    this.plantWateringStatusTracker[plantId] = false;
     if(this.wateringNow[plantId])
     {
-    //   setTimeout (() => {
-    //     console.log("Hello from setTimeout");
-    //  }, 1000);
       this.timeInterval[plantId] =  setInterval(()=>{
         this.timer[plantId] = this.timer[plantId] - 1
         if(this.timer[plantId] == 0)
           this.resetWatering(plantId)
         console.log(this.timer);
-      }, 1000);
+      }, this.timeToWater);
     }
     else{
       this.resetWatering(plantId)
-      // if(this.timeInterval[plantId]){
-      //   clearInterval(this.timeInterval[plantId]);
-      // }
-      // this.timer[plantId] = 10
-      // console.log(this.timer);
     }
   }
 
@@ -81,7 +92,7 @@ export class AppComponent {
     setTimeout (() => {
       this.buttonDisableTimer[plantId] = false;
       console.log("Hello from setTimeout");
-    }, 30000);
+    }, this.timeTodisableWateringAfterWatering);
     console.log(this.timer);
   }
 
